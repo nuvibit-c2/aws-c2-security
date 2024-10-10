6c8f7d6406863c29ea6c9a7347c04d7f000011b5
stefano-franco <stefano.franco@nuvibit.com>
2024-10-10 20:54:18 +0200

    delete security tooling

--- ntc_secutiy_tooling.tf
+++ ntc_secutiy_tooling.tf
@@ -2,6 +2,8 @@
 # ¦ NTC SECURITY TOOLING
 # ---------------------------------------------------------------------------------------------------------------------
 module "security_tooling" {
+  count = 0
+  
   # source = "github.com/nuvibit-terraform-collection/terraform-aws-ntc-security-tooling?ref=1.1.2"
   source = "github.com/nuvibit-terraform-collection/terraform-aws-ntc-security-tooling?ref=feat-central-sechub"
 
