119e783d00941aca4f17bc23e504042bf5592ca2
stefano-franco <stefano.franco@nuvibit.com>
2025-01-22 15:31:27 +0100

    update source

--- ntc_security_tooling_regional.tf
+++ ntc_security_tooling_regional.tf
@@ -15,7 +15,7 @@ import {
 # ---------------------------------------------------------------------------------------------------------------------
 module "ntc_regional_security_config_euc1" {
   # source = "github.com/nuvibit-terraform-collection/terraform-aws-ntc-security-tooling//modules/regional-security-config?ref=1.3.1"
-  source = "github.com/nuvibit-terraform-collection/terraform-aws-ntc-security-tooling//modules/regional-security-config?ref=feat-iam-access-analyzer"
+  source = "github.com/nuvibit-terraform-collection/terraform-aws-ntc-security-tooling//modules/regional-security-config?ref=feat-access-analyzer"
 
   # https://docs.aws.amazon.com/guardduty/latest/ug/guardduty_regions.html
   guardduty_config = {
