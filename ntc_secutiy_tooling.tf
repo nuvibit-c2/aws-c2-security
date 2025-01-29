f1dc66b4b21b9ddbf3cc3410b94e2656a375b573
stefano-franco <stefano.franco@nuvibit.com>
2025-01-29 15:03:21 +0100

    chore: upgrad security tooling

--- ntc_secutiy_tooling.tf
+++ ntc_secutiy_tooling.tf
@@ -11,7 +11,7 @@ import {
 # ¦ NTC SECURITY TOOLING
 # ---------------------------------------------------------------------------------------------------------------------
 module "ntc_security_tooling" {
-  source = "github.com/nuvibit-terraform-collection/terraform-aws-ntc-security-tooling?ref=1.4.1"
+  source = "github.com/nuvibit-terraform-collection/terraform-aws-ntc-security-tooling?ref=1.5.0"
 
   # set to true to enable securityhub standards that securityhub has designated as automatically enabled
   # use 'securityhub_central_configuration_polices' to configure security standards across entire aws organizations
