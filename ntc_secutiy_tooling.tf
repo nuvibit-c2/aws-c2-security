17ce1f5ea92599bd6b700ab4ae698da583d10ecf
stefano-franco <stefano.franco@nuvibit.com>
2025-04-28 22:52:16 +0200

    remove policies

--- ntc_secutiy_tooling.tf
+++ ntc_secutiy_tooling.tf
@@ -86,66 +86,66 @@ module "ntc_security_tooling" {
   # define securityhub central configuration policies
   # https://docs.aws.amazon.com/securityhub/latest/userguide/central-configuration-intro.html
   securityhub_central_configuration_polices = [
-    {
-      name        = "ntc-securityhub-central-policy"
-      description = "securityhub central policy"
-      # enable or disable securityhub in target accounts
-      enable_securityhub = true
-      # policy targets can either be organizational units (OU) or aws accounts (ID)
-      policy_targets = [
-        local.ntc_parameters["mgmt-account-factory"]["core_accounts"]["aws-c2-management"],
-        local.ntc_parameters["mgmt-organizations"]["ou_ids"]["/root/core"],
-        local.ntc_parameters["mgmt-organizations"]["ou_ids"]["/root/workloads"],
+    # {
+    #   name        = "ntc-securityhub-central-policy"
+    #   description = "securityhub central policy"
+    #   # enable or disable securityhub in target accounts
+    #   enable_securityhub = true
+    #   # policy targets can either be organizational units (OU) or aws accounts (ID)
+    #   policy_targets = [
+    #     local.ntc_parameters["mgmt-account-factory"]["core_accounts"]["aws-c2-management"],
+    #     local.ntc_parameters["mgmt-organizations"]["ou_ids"]["/root/core"],
+    #     local.ntc_parameters["mgmt-organizations"]["ou_ids"]["/root/workloads"],
 
-        # policy will not be rolled out to sandbox accounts and to suspended or accounts in transition
-        # local.ntc_parameters["mgmt-organizations"]["ou_ids"]["/root/sandbox"],
-        # local.ntc_parameters["mgmt-organizations"]["ou_ids"]["/root/suspended"],
+    #     # policy will not be rolled out to sandbox accounts and to suspended or accounts in transition
+    #     # local.ntc_parameters["mgmt-organizations"]["ou_ids"]["/root/sandbox"],
+    #     # local.ntc_parameters["mgmt-organizations"]["ou_ids"]["/root/suspended"],
 
-        # local.ntc_parameters["mgmt-organizations"]["ou_ids"]["/root/transition"],
-        # to apply policy to all accounts in organization use /root parent ou as target
-        # local.ntc_parameters["mgmt-organizations"]["ou_ids"]["/root"]
-      ]
-      enabled_standards = [
-        "aws-foundational-security-best-practices/v/1.0.0",
-        "cis-aws-foundations-benchmark/v/3.0.0",
-        # "cis-aws-foundations-benchmark/v/1.4.0",
-        # "cis-aws-foundations-benchmark/v/1.2.0",
-        # "nist-800-53/v/5.0.0",
-        # "pci-dss/v/3.2.1",
-      ]
-      # either provide a list of control ids which should be enabled (all other existing and future controls will be disabled)
-      enabled_control_ids = []
-      # or a list of control ids which should be disabled (all other existing and future controls will be enabled)
-      # https://docs.aws.amazon.com/securityhub/latest/userguide/securityhub-controls-reference.html
-      disabled_control_ids = [
-        "IAM.9",    # "MFA should be enabled for the root user"
-        "IAM.18",   # "Ensure a support role has been created to manage incidents with AWS Support"
-        "Config.1", # "AWS Config should be enabled"
-        "EC2.10",   # "Security groups should not allow ingress from 0.0.0.0/0 or ::/0 to port 22"
-        "IAM.6",    # "Hardware MFA should be enabled for the root user"
-        "S3.1"      # "S3 general purpose buckets should have block public access settings enabled"
-      ]
-      # some controls allow to customize parameters
-      # https://docs.aws.amazon.com/securityhub/latest/userguide/custom-control-parameters.html
-      # https://docs.aws.amazon.com/securityhub/latest/userguide/securityhub-controls-reference.html
-      customized_security_controls = [
-        {
-          control_id = "IAM.7"
-          parameters = [
-            {
-              name  = "MaxPasswordAge"
-              value = 60
-              type  = "Integer"
-            },
-            {
-              name  = "RequireNumbers"
-              value = false
-              type  = "Boolean"
-            }
-          ]
-        }
-      ]
-    }
+    #     # local.ntc_parameters["mgmt-organizations"]["ou_ids"]["/root/transition"],
+    #     # to apply policy to all accounts in organization use /root parent ou as target
+    #     # local.ntc_parameters["mgmt-organizations"]["ou_ids"]["/root"]
+    #   ]
+    #   enabled_standards = [
+    #     "aws-foundational-security-best-practices/v/1.0.0",
+    #     "cis-aws-foundations-benchmark/v/3.0.0",
+    #     # "cis-aws-foundations-benchmark/v/1.4.0",
+    #     # "cis-aws-foundations-benchmark/v/1.2.0",
+    #     # "nist-800-53/v/5.0.0",
+    #     # "pci-dss/v/3.2.1",
+    #   ]
+    #   # either provide a list of control ids which should be enabled (all other existing and future controls will be disabled)
+    #   enabled_control_ids = []
+    #   # or a list of control ids which should be disabled (all other existing and future controls will be enabled)
+    #   # https://docs.aws.amazon.com/securityhub/latest/userguide/securityhub-controls-reference.html
+    #   disabled_control_ids = [
+    #     "IAM.9",    # "MFA should be enabled for the root user"
+    #     "IAM.18",   # "Ensure a support role has been created to manage incidents with AWS Support"
+    #     "Config.1", # "AWS Config should be enabled"
+    #     "EC2.10",   # "Security groups should not allow ingress from 0.0.0.0/0 or ::/0 to port 22"
+    #     "IAM.6",    # "Hardware MFA should be enabled for the root user"
+    #     "S3.1"      # "S3 general purpose buckets should have block public access settings enabled"
+    #   ]
+    #   # some controls allow to customize parameters
+    #   # https://docs.aws.amazon.com/securityhub/latest/userguide/custom-control-parameters.html
+    #   # https://docs.aws.amazon.com/securityhub/latest/userguide/securityhub-controls-reference.html
+    #   customized_security_controls = [
+    #     {
+    #       control_id = "IAM.7"
+    #       parameters = [
+    #         {
+    #           name  = "MaxPasswordAge"
+    #           value = 60
+    #           type  = "Integer"
+    #         },
+    #         {
+    #           name  = "RequireNumbers"
+    #           value = false
+    #           type  = "Boolean"
+    #         }
+    #       ]
+    #     }
+    #   ]
+    # }
   ]
 
   providers = {
