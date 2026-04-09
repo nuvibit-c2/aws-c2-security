059163783d5de6faf6d1bcc4b1af3131bc43a2a8
stefano-franco <stefano.franco@nuvibit.com>
2026-04-09 15:03:44 +0200

    refactor: remove redundant AWS provider configurations for eu-central and us-east regions

--- main.tf
+++ main.tf
@@ -8,31 +8,6 @@ provider "aws" {
   }
 }
 
-provider "aws" {
-  alias  = "euc1"
-  region = "eu-central-1"
-  default_tags {
-    tags = local.default_tags
-  }
-}
-
-provider "aws" {
-  alias  = "euc2"
-  region = "eu-central-2"
-  default_tags {
-    tags = local.default_tags
-  }
-}
-
-# provider for us-east-1 region is sometimes required for specific features or services
-provider "aws" {
-  alias  = "use1"
-  region = "us-east-1"
-  default_tags {
-    tags = local.default_tags
-  }
-}
-
 # ---------------------------------------------------------------------------------------------------------------------
 # ¦ REQUIREMENTS
 # ---------------------------------------------------------------------------------------------------------------------
